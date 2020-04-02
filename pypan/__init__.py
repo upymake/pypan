20596b1873316efd9b31421173ed66b51bb45636
Volodymyr Yahello <vyahello@gmail.com>
2020-04-03 00:26:52 +0300

    Introduce initial version of a project

---
+++ pypan/__init__.py
@@ -0,0 +1,5 @@
+"""Serves command-line utility that creates python projects from certain based template."""
+
+__author__: str = "Volodymyr Yahello"
+__email__: str = "vyahello@gmail.com"
+__version__: str = "0.0.0"
